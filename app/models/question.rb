2c8cc5e907e295360ce89fefc9e7046624bab0f8
Stephen Dettling <sdettling@gmail.com>
2013-03-10 15:00:00 -0400

    added nested forms, added haml

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
-  attr_accessible :max_selections, :min_selections, :query, :ranked
+  attr_accessible :max_selections, :min_selections, :query, :ranked, :question_options_attributes
   belongs_to :user
   has_many :question_options
+  accepts_nested_attributes_for :question_options, allow_destroy: true
 end
