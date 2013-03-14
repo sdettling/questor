f9ffa5a7a996e6dae24a7603641c2c0e1d27669a
Stephen Dettling <sdettling@gmail.com>
2013-03-14 19:50:44 -0400

    fixed last migration

--- db/migrate/20130313051727_update.rb
+++ db/migrate/20130313051727_update.rb
@@ -1,11 +1,11 @@
 class Update < ActiveRecord::Migration
   def up
     add_column :answers, :question_option_id, :integer
-    remove_column :answers, :question_id, :integer
+    remove_column :answers, :question_id
   end
 
   def down
     add_column :answers, :question_id, :integer
-    remove_column :answers, :question_option_id, :integer
+    remove_column :answers, :question_option_id
   end
 end
